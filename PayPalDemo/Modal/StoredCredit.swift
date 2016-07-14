9a67f311f8b99895ba57c90bdecfee56a9c72af9
Sandeep Kharbanda <sandeep.kharbanda@tothenew.com>
2016-07-15 04:56:04 +0530

    Implemented Stored credit changes

--- PayPalDemo/Modal/StoredCredit.swift
+++ PayPalDemo/Modal/StoredCredit.swift
@@ -10,20 +10,20 @@ import UIKit
 
 class StoredCredit {
     
-    var billing_address : String?
-    var create_time: String?
+    var billingAddress : BillingAddress?
+    var createTime: String?
     var expireMonth: String!
     var expireYear: String!
     var firstName: String!
     var id : String!
-    var last_name: String!
-    var links: String?
+    var lastName: String!
+    var links: Array<Links>?
     var number: String!
-    var payer_id: String!
+    var payerId: String!
     var state: String?
     var type: String!
     var updateTime: String?
-    var valid_until: String?
+    var validUntil: String?
     
     init(){
         
@@ -32,9 +32,133 @@ class StoredCredit {
     
     init(info:AnyObject){
         if info is Dictionary<String, AnyObject> {
-            var creditInfo = info as! Dictionary<String, AnyObject>
+            let creditInfo = info as! Dictionary<String, AnyObject>
+            
+            if let _ = creditInfo["billing_address"]{
+                self.billingAddress = BillingAddress(info: creditInfo["billing_address"]!)
+                
+            }
+            
+            if let _ = creditInfo["create_time"]{
+                self.createTime = creditInfo["create_time"] as? String
+                
+            }
+            
+            self.expireMonth = creditInfo["expire_month"] as! String
+            self.expireYear = creditInfo["expire_year"] as! String
+            self.firstName = creditInfo["first_name"] as! String
+            self.id = creditInfo["id"] as! String
+            self.lastName = creditInfo["last_name"] as! String
+            
+            if let _ = creditInfo["payer_id"]{
+                self.payerId = creditInfo["payer_id"] as? String
+            }
+            
+            self.number = creditInfo["number"] as! String
+            
+            if let _ = creditInfo["links"]{
+                let infoLinks = creditInfo["links"]
+                
+                if  infoLinks is Array<Dictionary<String,AnyObject>> {
+                    let cardLinks = infoLinks as! Array<Dictionary<String,AnyObject>>
+                    
+                    self.links = Array<Links>()
+                    for (_, link) in cardLinks.enumerate() {
+                        self.links?.append(Links(info: link))
+                    }
+                }
+            }
+            
+            if let _ = creditInfo["state"]{
+                self.state = creditInfo["state"] as? String
+            }
+            
+            
+            if let _ = creditInfo["type"]{
+                self.type = creditInfo["type"] as? String
+            }
+            
+            if let _ = creditInfo["update_time"]{
+                self.updateTime = creditInfo["update_time"] as? String
+            }
+            
+            if let _ = creditInfo["valid_until"]{
+                self.validUntil = creditInfo["valid_until"] as? String
+            }
+            
             
         }
     }
+    
+}
 
+class BillingAddress {
+    
+    var city : String?
+    var countryCode : String?
+    var line1 : String?
+    var postalCode : String?
+    var state: String?
+    
+    init(){
+        
+    }
+    
+    
+    init(info:AnyObject){
+        if info is Dictionary<String, AnyObject> {
+            let billingInfo = info as! Dictionary<String, AnyObject>
+            
+            if let _ = billingInfo["city"]{
+                self.city = billingInfo["city"] as? String
+            }
+            
+            if let _ = billingInfo["country_code"]{
+                self.countryCode = billingInfo["country_code"] as? String
+            }
+            
+            if let _ = billingInfo["line1"]{
+                self.line1 = billingInfo["line1"] as? String
+            }
+            
+            if let _ = billingInfo["postal_code"]{
+                self.postalCode = billingInfo["postal_code"] as? String
+            }
+            
+            if let _ = billingInfo["state"]{
+                self.state = billingInfo["state"] as? String
+            }
+            
+        }
+    }
+}
+
+class Links {
+    
+    var href : String?
+    var method : String?
+    var rel : String?
+    
+    init(){
+        
+    }
+    
+    init(info:AnyObject){
+        if info is Dictionary<String, AnyObject> {
+            let linkInfo = info as! Dictionary<String, AnyObject>
+            
+            if let _ = linkInfo["href"]{
+                self.href = linkInfo["href"] as? String
+            }
+            
+            if let _ = linkInfo["method"]{
+                self.method = linkInfo["method"] as? String
+            }
+            
+            if let _ = linkInfo["rel"]{
+                self.rel = linkInfo["rel"] as? String
+            }
+            
+        }
+    }
 }
--- PayPalDemo/StoredCardViewController.swift
+++ PayPalDemo/StoredCardViewController.swift
@@ -9,15 +9,19 @@
 import UIKit
 import Alamofire
 
-class StoredCardViewController: UIViewController {
+class StoredCardViewController: UIViewController, UITextFieldDelegate {
     
     
     @IBOutlet weak var priceLbl: UILabel!
+    @IBOutlet weak var cardTxtFld: UITextField!
+    @IBOutlet weak var cardsTblVw: UITableView!
+    
     
     var accessToken: String = ""
     
     var price : String!
     
+    var storedCards = Array<StoredCredit>()
     
     override func viewDidLoad() {
         
@@ -91,13 +95,49 @@ class StoredCardViewController: UIViewController {
                     
                     if result is Dictionary<String, AnyObject>{
                         var storedCardResults = result as! Dictionary<String, AnyObject>
-                        print(storedCardResults["items"])
+                        
+                        let items = storedCardResults["items"]
+                        if items is Array<Dictionary<String, AnyObject>>{
+                            let cardItems = items as! Array<Dictionary<String, AnyObject>>
+                            for(_, card) in cardItems.enumerate(){
+                                self.storedCards.append(StoredCredit(info: card))
+                            }
+                            
+                            self.cardsTblVw.reloadData()
+                        }
                     }
                 }
                 
                 
         }
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return storedCards.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("cardCell")
+        
+        if cell == nil{
+            cell = UITableViewCell(style: .Default, reuseIdentifier: "cardCell")
+        }
+        
+        cell!.textLabel?.text = storedCards[indexPath.row].number
+        return cell!
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        cardTxtFld.text = storedCards[indexPath.row].number
+        self.cardsTblVw.hidden = true
+    }
+    
+    
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        self.cardsTblVw.hidden = false
+        return false
+    }
+    
     
     /*
      // MARK: - Navigation
