0447ab2e331281398b6f872be4edfcbbe0ea2a22
Deepak.Srivastava <ttnd@TTNDs-MacBook-Pro-3.local>
2016-07-15 02:46:45 +0530

    first commit

---
+++ PayPalDemo/CreditCardModal.swift
@@ -0,0 +1,13 @@
+//
+//  CreditCardModal.swift
+//  PayPalDemo
+//
+//  Created by Sandeep Kharbanda on 15/07/16.
+//  Copyright © 2016 Sandeep Kharbanda. All rights reserved.
+//
+
+import UIKit
+
+class CreditCardModal: NSObject {
+
+}
