4c265900ab7a41d730f21505782a15480583e379
Deepak.Srivastava <ttnd@TTNDs-MacBook-Pro-3.local>
2016-07-15 03:45:55 +0530

    added UI changes

---
+++ PayPalDemo/UIView.swift
@@ -0,0 +1,42 @@
+//
+//  UIView.swift
+//  AerocinemaTV
+//
+//  Created by Deepak Srivastava on 06/06/16.
+//  Copyright © 2016 TTND. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable extension UIView {
+    @IBInspectable var borderColor:UIColor? {
+        set {
+            layer.borderColor = newValue!.CGColor
+        }
+        get {
+            if let color = layer.borderColor {
+                return UIColor(CGColor:color)
+            }
+            else {
+                return nil
+            }
+        }
+    }
+    @IBInspectable var borderWidth:CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    @IBInspectable var cornerRadius:CGFloat {
+        set {
+            layer.cornerRadius = newValue
+            clipsToBounds = newValue > 0
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+}
